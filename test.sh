9759f06296472f15e7e4b5c29630204a9f4cefe6
Gustavo Sarraf de Souza <gugasarraf@gmail.com>
2022-09-15 20:36:39 -0300

    Fixed comparison

--- test.sh
+++ test.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 # Configs
-title='E'
-code="2018/E/E.py"
-input_dir="2018/E/input"
-output_dir="2018/E/output"
+title='B'
+code="2018/warmup_B/B.py"
+input_dir="2018/warmup_B/input"
+output_dir="2018/warmup_B/output"
 
 file_count=$(ls $input_dir | wc -l)
 
 for ((l=1;l<=file_count;l++))
 do
-	if [ "$(python $code < $input_dir/$title'_'$l)" -eq "$(cat $output_dir/$title'_'$l)" ]; then
-		echo "$l Valid"
+	if [ "$(python $code < $input_dir/$title'_'$l)" = "$(< $output_dir/$title'_'$l)" ]; then
+		printf "%d\tValid\n" "$l"
 	else
-		echo "$l Invalid"
+		printf "%d\tInvalid\n" "$l"
 	fi
 done
